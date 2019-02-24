48e2255ef6d71b87e5ce21d516ad77deab697675
crsschwalm <cschwalm@indiana.edu>
2019-02-24 18:53:58 -0500

    add placeholder content

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1></h1>
+    <p>Welcome to my personal website built with <a href="https://www.gatsbyjs.org">GatsbyJS</a></p>
+    <p>Stay tuned as I update this project :)</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
