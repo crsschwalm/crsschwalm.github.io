63fe6ddcd79d9e81edbd6b9df087a35bdc92cd68
crsschwalm <cschwalm@indiana.edu>
2019-02-24 19:45:53 -0500

    update some placeholder content

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people, Im currently working on my profile page!</h1>
+    <p>While I work on this project, I'd encourage you to check out <a href="https://www.gatsbyjs.org">GatsbyJS</a></p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
