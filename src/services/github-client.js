a4eaa18ce476a6977f7ca85ec8cfea7b061104da
crsschwalm <cschwalm@indiana.edu>
2020-03-01 19:40:01 -0500

    prod api

--- src/services/github-client.js
+++ src/services/github-client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const apiURL = 'https://o9ik7ex0rd.execute-api.us-east-1.amazonaws.com/dev'
-// const apiURL = 'https://o9ik7ex0rd.execute-api.us-east-1.amazonaws.com/prod'
+// const apiURL = 'https://o9ik7ex0rd.execute-api.us-east-1.amazonaws.com/dev'
+const apiURL = 'https://mil3cwicj8.execute-api.us-east-1.amazonaws.com/prod'
 
 export const githubGQL = async (query) => {
     try {
